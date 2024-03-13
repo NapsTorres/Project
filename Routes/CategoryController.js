1d739848e3ccdb9c8d5ca315053e8849c48adae2
Napoleon Torres <ntorres@gbox.ncf.edu.ph>
2024-03-13 08:26:50 +0800

    Update

--- Routes/CategoryController.js
+++ Routes/CategoryController.js
@@ -9,17 +9,20 @@ const CategoryController = express.Router();
 // Category registration start
 CategoryController.post('/category_reg', authenticateToken, async (req, res) => {
     try {
-        const { CategoryName, UserID } = req.body;
+        // Extract the user ID from the request object after it's been populated by the authentication middleware
+        const UserID = req.userID;
+        const { CategoryName } = req.body;
 
         const insertCategoryQuery = 'INSERT INTO EventCategories (CategoryName, UserID) VALUES (?, ?)';
         await db.promise().execute(insertCategoryQuery, [CategoryName, UserID]);
 
         res.status(201).json({ message: 'Category registered successfully' });
     } catch (error) {
-        console.error('Error Category', error);
+        console.error('Error creating category:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 });
+
 // Category registration end
 
 // Fetch all categories
